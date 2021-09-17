2e80b657eda464e998fa19d465f4ea55c037af60
vcinonyx <vcinonyx@gmail.com>
2021-09-17 19:39:15 +0300

    Add Identity

---
+++ BLL/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,41 @@
+﻿using DAL.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLL.Identity
+{
+    public static class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Bob Marley",
+                    Email = "bobmarley@test.com",
+                    UserName = "bobmarlet@test.com",
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles = new string[] { "Admin", "Manager", "Programmer" };
+
+            foreach (string role in roles)
+            {
+                if (!roleManager.Roles.Any(r => r.Name == role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole { Name = "Admin" });
+                }
+            }
+
+        }
+
+    }
+}
